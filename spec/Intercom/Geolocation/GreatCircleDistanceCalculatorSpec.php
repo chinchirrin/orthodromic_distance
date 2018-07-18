e9cc92d5d98dadb9c3f56d9760f3021101867e9b
Andres Rojas Moran <arojasmoran@gmail.com>
2018-07-18 23:58:55 +0100

    Add test case for computing the distance between two gps coords

--- spec/Intercom/Geolocation/GreatCircleDistanceCalculatorSpec.php
+++ spec/Intercom/Geolocation/GreatCircleDistanceCalculatorSpec.php
@@ -14,5 +14,16 @@ class GreatCircleDistanceCalculatorSpec extends ObjectBehavior
         $this->degreesToRadians($degrees)
             ->shouldBeApproximately(0.9311, 1.0e-4);
     }
+
+    function it_calculates_the_distance_between_two_points()
+    {
+        // Spire in Dublin
+        $from = [53.34985936124078, -6.260243651751125];
+        // Titanic Belfast
+        $to = [54.6081729724316, -5.909649380798328];
+        //$this->shouldHaveType(GreatCircleDistanceCalculator::class);
+        $this->calculateDistance($from, $to)
+            ->shouldBeApproximately(141.78339, 1.0e-5);
+    }
 }
 
