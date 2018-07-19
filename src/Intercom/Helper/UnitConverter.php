5bd0f4b85ffce0467cb057816f4dfb86a82fe470
Andres Rojas Moran <arojasmoran@gmail.com>
2018-07-19 21:58:21 +0100

    Add validations to `degreesToRadians` method to handle invalid inputs.

--- src/Intercom/Helper/UnitConverter.php
+++ src/Intercom/Helper/UnitConverter.php
@@ -5,11 +5,18 @@ namespace Intercom\Helper;
 class UnitConverter
 {
     /**
-     * @param   float   $degrees
-     * @return  float
+     * Returns the give $degrees into radians units if a valid value is provided,
+     * null otherwise
+     *
+     * @param   float       $degrees
+     * @return  float|null
      */
     public function degreesToRadians($degrees)
     {
+        if (!is_numeric($degrees)) {
+            return null;
+        }
+
         $radians = $degrees * pi() / 180.0;
 
         return $radians;
